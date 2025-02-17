4c8ececf90fdbdae2bf4ecd9cb95754f34b76efc
jnakht <jisan.cse.cu@gmail.com>
2025-02-17 17:45:01 +0600

    visited button toggle added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,22 @@
+import { useState } from 'react';
 import './Country.css'
 
 const Country = ({country}) => {
-    const {name, flags, population, area} = country;
-    console.log(country)
+    const {name, flags, population, area, cca3} = country;
+    console.log(country);
+
+    const [visited, setVisited] = useState(false);
+    const handleVisitedButton = () => {
+        setVisited(!visited);
+    }
     return (
         <div className='country'>
             <h3>Name: {name.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
+            <p><small>Code: {cca3}</small></p>
+            <button onClick={handleVisitedButton}>{visited ? "Marked as Visited" : "Visited"}</button>
         </div>
     );
 };
